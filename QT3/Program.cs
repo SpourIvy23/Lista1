495460c12103dc284ba20abec15000bd1e928495
SpourIvy <rivy2462@gmail.com>
2024-08-20 11:50:55 -0300

    O codigo da terceira questão foi finalizado

--- QT3/Program.cs
+++ QT3/Program.cs
@@ -14,6 +14,26 @@ class Program
 {
     static void Main()
     {
-        
+        int dano, vida, regen;
+        double modHab, modEq, total;
+
+        System.Console.WriteLine("Escreva o total de vida");
+        int.TryParse(Console.ReadLine(), out vida);
+        System.Console.WriteLine("Escreva o total de dano recebido");
+         int.TryParse(Console.ReadLine(), out dano);
+         System.Console.WriteLine("Escreva o bonus de regeneração");
+         int.TryParse(Console.ReadLine(), out regen);
+         System.Console.WriteLine("Escreva o modificador de equipamento");
+         double.TryParse(Console.ReadLine(), out modEq);
+         System.Console.WriteLine("Escreva o modificador de habilidade");
+         double.TryParse(Console.ReadLine(), out modHab);
+
+         total = vida + regen - (dano * modEq * modHab);
+
+         System.Console.WriteLine("Sua vida total é: " + total);
+         
+         System.Console.WriteLine("Aperte ENTER para fechar");
+         Console.ReadLine();
+         
     }
 }
